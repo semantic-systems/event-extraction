fd39740b5f930b7b3bec230f9bd0438a42e4e698
Junbo Huang <junbo.huang@uni-hamburg.de>
2021-11-19 14:12:58 +0100

    test: hydra value can be null

--- tests/test_hydra.py
+++ tests/test_hydra.py
@@ -7,3 +7,8 @@ def test_nested_dict_in_hydra_is_iterable(initialize_hydra):
     assert isinstance(cfg, DictConfig)
     layers = [(layer.n_in, layer.n_out) for layer in cfg.model.layers.values()]
     assert layers == [(256, 128), (128, 12)]
+
+
+def test_empty_value(initialize_hydra):
+    cfg = compose(config_name="test_config_1.yml")
+    assert not cfg.data.testing_data_path
