b9c53268737f1e9e26801dfe3aa38701a1b4da95
Junbo Huang <junbo.huang@uni-hamburg.de>
2021-11-22 14:33:26 +0100

    added call method for preprocessor to return a dataloader instance

--- preprocessors/preprocessor.py
+++ preprocessors/preprocessor.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 from abc import abstractmethod
+from typing import Optional
+
 import pandas as pd
 from omegaconf import DictConfig
 from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, Dataset, Sampler, RandomSampler
 
 
 class Preprocessor(object):
@@ -22,3 +25,11 @@ class Preprocessor(object):
     @staticmethod
     def load_csv(path: Path) -> pd.DataFrame:
         return pd.read_csv(path)
+
+    def __call__(self,
+                 dataset: Dataset,
+                 batch_size: int,
+                 shuffle: Optional[bool] = False,
+                 sampler: Optional[Sampler] = RandomSampler) -> DataLoader:
+        return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, sampler=sampler)
+
