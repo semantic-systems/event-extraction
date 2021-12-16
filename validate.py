ab6adc5aa802fc45164a19889e70678a3c31282b
Junbo Huang <junbo.huang@uni-hamburg.de>
2021-12-16 17:55:13 +0100

    validator: create validator class to check config

--- validate.py
+++ validate.py
@@ -1,5 +1,6 @@
 # validate config value
 import logging
+from pathlib import Path
 
 from omegaconf import DictConfig
 
@@ -7,12 +8,8 @@ from omegaconf import DictConfig
 class ConfigValidator(object):
     def __init__(self, config: DictConfig):
         self.config = config
-        self.model = config.model
-        self.name = config.name
-        self.data = config.data
-        self.episode = config.episode
 
-    def validate(self):
+    def __call__(self):
         try:
             self.validate_model()
             self.validate_data()
@@ -22,12 +19,23 @@ class ConfigValidator(object):
             raise
 
     def validate_model(self):
-        pass
+        self.create_output_path()
 
     def validate_data(self):
         pass
 
     def validate_episode(self):
         pass
+
+    def create_output_path(self):
+        if not Path(self.config.model.output_path, self.config.name).absolute().exists():
+            logging.warning(f"Output path {str(Path(self.config.model.output_path, self.config.name).absolute())} "
+                            f"does not exist. It will be automatically created. ")
+            Path(self.config.model.output_path, self.config.name).absolute().mkdir(parents=True, exist_ok=True)
+        if not Path(self.config.model.output_path, self.config.name, "pretrained_models").absolute().exists():
+            Path(self.config.model.output_path, self.config.name, "pretrained_models").absolute().mkdir(parents=True, exist_ok=True)
+        if not Path(self.config.model.output_path, self.config.name, "plots").absolute().exists():
+            Path(self.config.model.output_path, self.config.name, "plots").absolute().mkdir(parents=True, exist_ok=True)
+
 
 
