db71d666da7864561de9141c9c7bbfd5babe82e6
Junbo Huang <junbo.huang@uni-hamburg.de>
2022-07-14 15:40:03 +0200

    main: added script to load configs from a dir

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import glob
 import logging
 import warnings
-
+from pathlib import Path
 from event_extractor.engines.trainer import MetaLearningTrainer, BatchLearningTrainer
 from event_extractor.parsers.parser import parse
 from utils import instantiate_config
@@ -19,9 +20,18 @@ if __name__ == "__main__":
     warnings.filterwarnings("ignore")
 
     args = parse()
-    cfg = instantiate_config(args.config, args.job_name)
-    trainer_class = get_trainer(args.config)
-    trainer = trainer_class(cfg)
-    trainer.run()
-
+    if Path(args.config).is_file():
+        cfg = instantiate_config(args.config, args.job_name)
+        trainer_class = get_trainer(args.config)
+        trainer = trainer_class(cfg)
+        trainer.run()
+    elif Path(args.config).is_dir():
+        configs = glob.glob(str(Path(args.config).absolute()) + "*.yaml")
+        for config in configs:
+            cfg = instantiate_config(config, args.job_name)
+            trainer_class = get_trainer(config)
+            trainer = trainer_class(cfg)
+            trainer.run()
+    else:
+        pass
 
