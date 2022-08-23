ad665701c959386c1f21b9541d722c3821109586
Junbo Huang <junbo.huang@uni-hamburg.de>
2022-08-23 17:52:30 +0300

    fix: head layer

--- event_extractor/models/heads/__init__.py
+++ event_extractor/models/heads/__init__.py
@@ -1,3 +1,3 @@
 from event_extractor.models.heads.head import Head
-from event_extractor.models.heads.linear_layer_head import LinearLayerHead
+from event_extractor.models.heads.linear_layer_head import DenseLayerHead, DenseLayerContrastiveHead
 from event_extractor.models.heads.prototypical_head import PrototypicalHead
--- event_extractor/models/heads/linear_layer_head.py
+++ event_extractor/models/heads/linear_layer_head.py
@@ -7,22 +7,65 @@ from event_extractor.schema import EncodedFeature, HeadOutput
 import torch.nn.functional as F
 
 
-class LinearLayerHead(Head):
+class DenseLayerHead(Head):
     def __init__(self, cfg: DictConfig):
-        super(LinearLayerHead, self).__init__()
+        super(DenseLayerHead, self).__init__()
         layer_stacks = [Linear(layer.n_in, layer.n_out) for layer in cfg.model.layers.values()]
+        self.head_type = "mlp" if len(layer_stacks) > 1 else "linear"
         self.classification_layer = ModuleList(layer_stacks)
         self.dropout = Dropout(p=cfg.model.dropout_rate)
+        self.l2_normalize = cfg.model.L2_normalize_encoded_feature
 
-    def forward(self, encoded_features: EncodedFeature) -> HeadOutput:
+    def forward(self, encoded_features: EncodedFeature, mode: str) -> HeadOutput:
         encoded_feature: tensor = encoded_features.encoded_feature
         output = encoded_feature
-        for i, layer in enumerate(self.classification_layer):
-            if i < len(self.classification_layer) - 1:
-                if encoded_features.labels is None:
-                    output = F.relu(layer(output))
+        if self.head_type == "linear":
+            output = self.classification_layer[0](output)
+            if self.l2_normalize:
+                norm = output.norm(p=2, dim=1, keepdim=True)
+                output = output.div(norm.expand_as(output))
+        else:
+            for i, layer in enumerate(self.classification_layer):
+                if i < len(self.classification_layer) - 1:
+                    if mode == "train":
+                        output = F.relu(self.dropout(layer(output)))
+                    elif mode in ["validation", "test"]:
+                        output = F.relu(layer(output))
                 else:
-                    output = F.relu(self.dropout(layer(output)))
-            else:
-                output = F.relu(layer(output))
+                    output = layer(output)
+                    if self.l2_normalize:
+                        norm = output.norm(p=2, dim=1, keepdim=True)
+                        output = output.div(norm.expand_as(output))
+        return HeadOutput(output)
+
+
+class DenseLayerContrastiveHead(Head):
+    def __init__(self, cfg: DictConfig):
+        super(DenseLayerContrastiveHead, self).__init__()
+        layer_stacks = [Linear(layer.n_in, layer.n_out) for layer in cfg.model.layers.values()]
+        self.head_type = "mlp" if len(layer_stacks) > 1 else "linear"
+        self.classification_layer = ModuleList(layer_stacks)
+        self.dropout = Dropout(p=cfg.model.dropout_rate)
+        self.l2_normalize = cfg.model.L2_normalize_encoded_feature
+
+    def forward(self, encoded_features: EncodedFeature, mode: str) -> HeadOutput:
+        encoded_feature: tensor = encoded_features.encoded_feature
+        output = encoded_feature
+        if self.head_type == "linear":
+            if self.l2_normalize:
+                norm = output.norm(p=2, dim=-1, keepdim=True)
+                output = output.div(norm.expand_as(output))
+            output = F.softmax(self.classification_layer[0](output))
+        else:
+            for i, layer in enumerate(self.classification_layer):
+                if i < len(self.classification_layer) - 1:
+                    if mode == "train":
+                        output = F.relu(self.dropout(layer(output)))
+                    elif mode in ["validation", "test"]:
+                        output = F.relu(layer(output))
+                else:
+                    if self.l2_normalize:
+                        norm = output.norm(p=2, dim=-1, keepdim=True)
+                        output = output.div(norm.expand_as(output))
+                    output = F.softmax(layer(output))
         return HeadOutput(output)
