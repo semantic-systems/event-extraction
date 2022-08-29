3eb1739a8e5e9e288e48e99ad77f231feb7ccd54
Junbo Huang <junbo.huang@uni-hamburg.de>
2022-08-29 13:57:46 +0300

    :bug: fix bug on validation step for contrastive learning. It should use CE loss for classification instead of contrastive loss.

--- event_extractor/models/SingleLabelSequenceClassification.py
+++ event_extractor/models/SingleLabelSequenceClassification.py
@@ -81,10 +81,7 @@ class SingleLabelContrastiveSequenceClassification(SingleLabelSequenceClassifica
             return SingleLabelClassificationForwardOutput(loss=total_loss, prediction_logits=head_output.output)
         elif mode == "validation":
             loss = self.loss(head_output.output, input_feature.labels)
-            contrastive_features = head_output.output[:, None, :]
-            contrastive_loss = self.contrastive_loss(contrastive_features, input_feature.labels)
-            total_loss = (1 - self.contrastive_loss_ratio) * loss + self.contrastive_loss_ratio * contrastive_loss
-            return SingleLabelClassificationForwardOutput(loss=total_loss, prediction_logits=head_output.output)
+            return SingleLabelClassificationForwardOutput(loss=loss, prediction_logits=head_output.output)
         elif mode == "test":
             return SingleLabelClassificationForwardOutput(prediction_logits=head_output.output)
         else:
