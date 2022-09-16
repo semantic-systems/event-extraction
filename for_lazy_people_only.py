9b7be1840332a44242dc3fd4013bbdaa8241beee
Junbo Huang <junbo.huang@uni-hamburg.de>
2022-09-16 11:22:56 +0200

    :sparkles: update configs

--- for_lazy_people_only.py
+++ for_lazy_people_only.py
@@ -286,16 +286,17 @@ class ConfigWriter(object):
         for file in files:
             config = ConfigWriter.read_yaml(file)
             # config["seed"] = [0, 1, 2]
-            # config["model"]["output_path"] = "./outputs/tweeteval/experiments/preprocessed_data/contrastive_loss_ratio/09/"
-            config["model"]["contrastive"]["contrastive_loss_ratio"] = 0.3
+            # config["data"]["batch_size"] = 64
+            config["model"]["output_path"] = "./outputs/tweeteval/experiments/preprocessed_data/contrast_mode_one/"
+            config["model"]["contrastive"]["contrast_mode"] = "one"
             updated_dicts.append(config)
             ConfigWriter.write_from_dict(config, file)
 
 
 if __name__ == "__main__":
-    # ConfigWriter.change_field_of_all("./event_extractor/configs/tweeteval/experiments/contrastive_loss_ratio/03/")
-    writer = LatexTableWriter("./tables/tweeteval/contrastive_loss_ratio/dropout/preprocessed/", TweetEvalResultTable, TweetEvalResult)
-    writer.write_to_tex()
+    ConfigWriter.change_field_of_all("./event_extractor/configs/tweeteval/experiments/contrast_mode_one/")
+    # writer = LatexTableWriter("./tables/tweeteval/contrastive_loss_ratio/dropout/preprocessed/", TweetEvalResultTable, TweetEvalResult)
+    # writer.write_to_tex()
     # writer = LatexTableWriter("./tables/crisis/", CrisisResultTable, CrisisResult)
     # writer.write_to_tex()
 
