bbb519689b4b51f29fdccf5b9ca069ce039ebc50
Junbo Huang <junbo.huang@uni-hamburg.de>
2022-09-19 15:23:05 +0200

    :sparkles: added OOS data for EE task

--- event_extractor/data_generators/data_generator.py
+++ event_extractor/data_generators/data_generator.py
@@ -1,6 +1,6 @@
 from typing import Optional, Dict
 
-from datasets import load_dataset
+from datasets import load_dataset, concatenate_datasets, ClassLabel
 from omegaconf import DictConfig
 from torch.utils.data import DataLoader, Sampler, RandomSampler
 
@@ -17,6 +17,12 @@ class DataGenerator(object):
         dataset = load_dataset(self.cfg.data.name, self.cfg.data.config, split='train')
         if self.cfg.data.label_column != 'label':
             dataset = self.rename_label_column(dataset, self.cfg.data.label_column, 'label')
+        if "TRECIS_event_type" in self.cfg.data.name:
+            dataset_with_oos = concatenate_datasets([dataset, self.oos_dataset("train")])
+            new_features = dataset_with_oos.features.copy()
+            new_features["label"] = ClassLabel(names=dataset_with_oos.features["label"].names + ["oos"],
+                                               num_classes=dataset_with_oos.features["label"].num_classes + 1)
+            dataset = dataset_with_oos.cast(new_features)
         return dataset
 
     @property
@@ -24,6 +30,12 @@ class DataGenerator(object):
         dataset = load_dataset(self.cfg.data.name, self.cfg.data.config, split='validation')
         if self.cfg.data.label_column != 'label':
             dataset = self.rename_label_column(dataset, self.cfg.data.label_column, 'label')
+        if "TRECIS_event_type" in self.cfg.data.name:
+            dataset_with_oos = concatenate_datasets([dataset, self.oos_dataset("validation")])
+            new_features = dataset_with_oos.features.copy()
+            new_features["label"] = ClassLabel(names=dataset_with_oos.features["label"].names + ["oos"],
+                                               num_classes=dataset_with_oos.features["label"].num_classes + 1)
+            dataset = dataset_with_oos.cast(new_features)
         return dataset
 
     @property
@@ -31,7 +43,37 @@ class DataGenerator(object):
         dataset = load_dataset(self.cfg.data.name, self.cfg.data.config, split='test')
         if self.cfg.data.label_column != 'label':
             dataset = self.rename_label_column(dataset, self.cfg.data.label_column, 'label')
+        if "TRECIS_event_type" in self.cfg.data.name:
+            dataset_with_oos = concatenate_datasets([dataset, self.oos_dataset("test")])
+            new_features = dataset_with_oos.features.copy()
+            new_features["label"] = ClassLabel(names=dataset_with_oos.features["label"].names + ["oos"],
+                                               num_classes=dataset_with_oos.features["label"].num_classes + 1)
+            dataset = dataset_with_oos.cast(new_features)
         return dataset
+
+    @staticmethod
+    def oos_dataset(mode: str):
+        def change_label(example):
+            example["label"] = 9
+            return example
+        datasets = [load_dataset("tweet_eval", "emoji", split=mode),
+                    load_dataset("tweet_eval", "emotion", split=mode),
+                    load_dataset("tweet_eval", "sentiment", split=mode),
+                    load_dataset("tweet_eval", "hate", split=mode),
+                    load_dataset("tweet_eval", "offensive", split=mode),
+                    load_dataset("tweet_eval", "irony", split=mode),
+                    load_dataset("tweet_eval", "stance_abortion", split=mode),
+                    load_dataset("tweet_eval", "stance_atheism", split=mode),
+                    load_dataset("tweet_eval", "stance_climate", split=mode),
+                    load_dataset("tweet_eval", "stance_feminist", split=mode),
+                    load_dataset("tweet_eval", "stance_hillary", split=mode)
+                    ]
+        concatenated_dataset = concatenate_datasets(datasets)
+        updated_dataset = concatenated_dataset.map(change_label)
+        new_features = updated_dataset.features.copy()
+        new_features["label"] = ClassLabel(names=["oos"], num_classes=1)
+        updated_dataset = updated_dataset.cast(new_features)
+        return updated_dataset
 
     @staticmethod
     def rename_label_column(dataset, original_label_name, new_label_name):
