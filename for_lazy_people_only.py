4d2099512823667c1f591e6b2497a57f61e99d93
Junbo Huang <junbo.huang@uni-hamburg.de>
2022-11-15 14:57:29 +0100

    :sparkle: update config

--- for_lazy_people_only.py
+++ for_lazy_people_only.py
@@ -471,12 +471,12 @@ class ConfigWriter(object):
 
 
 if __name__ == "__main__":
-    ConfigWriter.change_field_of_all("event_extractor/configs/tweeteval/final/cohort7/09/")
-    # writer = LatexTableWriter("./tables/tweeteval/contrastive_learning_tweeteval/cohort7/", TweetEvalResult, table=TweetEvalMainTable)
+    # ConfigWriter.change_field_of_all("event_extractor/configs/tweeteval/final/cohort7/09/")
+    writer = LatexTableWriter("./tables/tweeteval/contrastive_learning_tweeteval/cohort4/", TweetEvalResult, table=TweetEvalMainTable)
     # writer.write_to_tex(name="encoded_feature_silhouette", session_to_include=["model", "contrastive_loss_ratio"], col_to_write="encoded_feature_silhouette")
     # writer.write_to_tex(name="final_output_silhouette", session_to_include=["model", "contrastive_loss_ratio"], col_to_write="final_output_silhouette")
     # writer.write_to_tex(name="tweeteval", session_to_include=["model"])
-    # writer.write_to_tex(name="tweeteval", session_to_include=["model", "contrastive_loss_ratio"])
+    writer.write_to_tex(name="tweeteval", session_to_include=["model", "contrastive_loss_ratio"])
     # writer = LatexTableWriter("./tables/tweeteval/paper/cohort2", TweetEvalResult, table=TweetEvalMainTable)
     # writer.write_to_tex(name="tweeteval", session_to_include=["model"])
 
