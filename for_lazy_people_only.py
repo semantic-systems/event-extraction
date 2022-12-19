750a13766856362dbcc558dd3ee0a9a4247be8a4
Junbo Huang <junbo.huang@uni-hamburg.de>
2022-12-20 04:14:38 +0800

    :sparkle: ablation

--- for_lazy_people_only.py
+++ for_lazy_people_only.py
@@ -455,24 +455,24 @@ class ConfigWriter(object):
             # output = config["model"]["output_path"]
             # updated_output = output.replace("/contrastive_loss_ratio/", "/base_temp/")
             # config["model"]["output_path"] = updated_output
-            config["early_stopping"]["tolerance"] = 5
+            # config["early_stopping"]["tolerance"] = 5
             # config["model"]["epochs"] = 100
             # config["data"]["gradient_accu_step"] = 1
-            config["data"]["batch_size"] = 32
+            # config["data"]["batch_size"] = 32
             # config["model"]["output_path"] = "./outputs/tweeteval/experiments/cohort7/09/bertweet/"
             # config["model"]["from_pretrained"] = "roberta-base"
-            config["model"]["L2_normalize_encoded_feature"] = True
-            config["model"]["L2_normalize_logits"] = True
+            # config["model"]["L2_normalize_encoded_feature"] = True
+            # config["model"]["L2_normalize_logits"] = True
             # config["model"]["learning_rate"] = 1.0e-05
             # config["augmenter"]["name"] = "dropout"
             # config["augmenter"]["dropout"] = [0.1, 0.1]
             # config["augmenter"]["num_samples"] = 2
             # config["model"]["contrastive"]["contrastive_loss_ratio"] = 0.9
             # config["model"]["contrastive"]["base_temperature"] = 0.3
-            config["model"]["contrastive"]["temperature"] = 0.03
+            # config["model"]["contrastive"]["temperature"] = 0.03
             # config["model"]["freeze_transformer_layers"] = "all"
             output = config["model"]["output_path"]
-            updated_output = output.replace("cohort10/aug/05/07/roberta_base/", "cohort10/aug/05/003/roberta_base/")
+            updated_output = output.replace("cohort10/aug/", "cohort10/ablation_aug/")
             config["model"]["output_path"] = updated_output
             # if updated_output.endswith("/"):
             #     updated_output = updated_output[:-1]
@@ -483,12 +483,12 @@ class ConfigWriter(object):
 
 
 if __name__ == "__main__":
-    ConfigWriter.change_field_of_all("event_extractor/configs/tweeteval/final/cohort10/augmentation/05/003/")
+    # ConfigWriter.change_field_of_all("event_extractor/configs/tweeteval/final/cohort10/augmentation/05/")
     # ConfigWriter.change_field_of_all("event_extractor/configs/tweeteval/final/cohort7/")
-    # writer = LatexTableWriter("./tables/tweeteval/contrastive_learning_tweeteval/cohort9/", TweetEvalResult, table=TweetEvalMainTable)
+    writer = LatexTableWriter("./tables/tweeteval/contrastive_learning_tweeteval/cohort10/aug/", TweetEvalResult, table=TweetEvalMainTable)
     # writer.write_to_tex(name="encoded_feature_silhouette", session_to_include=["model", "contrastive_loss_ratio"], col_to_write="encoded_feature_silhouette")
     # writer.write_to_tex(name="final_output_silhouette", session_to_include=["model", "contrastive_loss_ratio"], col_to_write="final_output_silhouette")
-    # writer.write_to_tex(name="tweeteval", session_to_include=["model", "contrastive_loss_ratio", "contrastive_temperature"])
+    writer.write_to_tex(name="tweeteval", session_to_include=["model", "contrastive_temperature"])
     # writer.write_to_tex(name="tweeteval", session_to_include=["model", "contrastive_loss_ratio"])
     # writer = LatexTableWriter("./tables/tweeteval/paper/cohort2", TweetEvalResult, table=TweetEvalMainTable)
     # writer.write_to_tex(name="tweeteval", session_to_include=["model"])
