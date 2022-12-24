:bug: fix variance / covariance calculation error: aggregation of the wrong dimension

event_extractor/losses/variance_maximization_covariance_minimization_loss.py
@@ -1,3 +1,5 @@
+from distutils import dist
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -40,17 +42,19 @@ class VarianceMaximizationCovarianceMinimizationLoss(nn.Module):
         classes = list(multiview_features_per_class.keys())
         loss = 0
         for c in classes:
-            std_per_class_in_view = torch.var(multiview_features_per_class[c], unbiased=True)
-            loss += torch.mean(F.relu(1 - std_per_class_in_view))
+            normalized_features_per_class = multiview_features_per_class[c] - multiview_features_per_class[c].mean(dim=0)
+            std_features_per_class = torch.sqrt(normalized_features_per_class.var(dim=0) + 0.0001)
+            loss += torch.mean(F.relu(1 - std_features_per_class))
         return loss
 
     def cov_loss(self, multiview_features_per_class, device):
         classes = list(multiview_features_per_class.keys())
         loss = 0
         for c in classes:
-            cov_per_class_in_view = torch.cov(multiview_features_per_class[c]).to(device)
-            loss += self.off_diagonal(cov_per_class_in_view).pow_(2).sum().div(
-                multiview_features_per_class[c].shape[-1])
+            normalized_features_per_class = multiview_features_per_class[c] - multiview_features_per_class[c].mean(dim=0)
+            size = normalized_features_per_class.shape[0]
+            cov = (normalized_features_per_class.T @ normalized_features_per_class) / (size - 1)
+            loss += self.off_diagonal(cov).pow_(2).sum().div(normalized_features_per_class.shape[-1])
         return loss
 
     def forward(self, features, labels):

for_lazy_people_only.py
@@ -37,6 +37,8 @@ class Result(object):
         self.contrastive_loss_ratio = 0 if not self.contrastive else self.config.get("model").get("contrastive").get("contrastive_loss_ratio")
         self.base_temperature = 0 if not self.contrastive else self.config.get("model").get("contrastive").get("base_temperature")
         self.contrastive_temperature = 0 if not self.contrastive else self.config.get("model").get("contrastive").get("temperature")
+        self.margin = self.config.get("loss").get("var_cov_loss").get("margin")
+        self.coeff = self.config.get("loss").get("var_cov_loss").get("coef")
         self.contrast_mode = np.nan if not self.contrastive else self.config.get("model").get("contrastive").get("contrast_mode")
         self.augmenter = self.get_augmenter()
         self.num_augmented_samples = self.config.get("augmenter").get("num_samples", None) if self.augmenter else np.nan
@@ -400,7 +402,9 @@ class LatexTableWriter(object):
                 'final_output_silhouette': [result.final_output_silhouette for result in result_instances],
                 'augmenter_dropout': [result.augmenter_dropout for result in result_instances],
                 'metric_name': metric_name,
-                'metric_score': [result.metric for result in result_instances]
+                'metric_score': [result.metric for result in result_instances],
+                'margin': [result.margin for result in result_instances],
+                'coeff': [result.coeff for result in result_instances]
                 }
         return pd.DataFrame(data)
 
@@ -486,10 +490,10 @@ class ConfigWriter(object):
 if __name__ == "__main__":
     # ConfigWriter.change_field_of_all("event_extractor/configs/tweeteval/final/cohort11/coef/9/")
     # ConfigWriter.change_field_of_all("event_extractor/configs/tweeteval/final/cohort7/")
-    writer = LatexTableWriter("./tables/tweeteval/contrastive_learning_tweeteval/cohort11/lecun", TweetEvalResult, table=TweetEvalMainTable)
+    writer = LatexTableWriter("./tables/tweeteval/contrastive_learning_tweeteval/cohort11/", TweetEvalResult, table=TweetEvalMainTable)
     # writer.write_to_tex(name="encoded_feature_silhouette", session_to_include=["model", "contrastive_loss_ratio"], col_to_write="encoded_feature_silhouette")
     # writer.write_to_tex(name="final_output_silhouette", session_to_include=["model", "contrastive_loss_ratio"], col_to_write="final_output_silhouette")
-    writer.write_to_tex(name="tweeteval", session_to_include=["model" , "contrastive_temperature"])
+    writer.write_to_tex(name="tweeteval", session_to_include=["model", "margin", "coeff"])
     # writer.write_to_tex(name="tweeteval", session_to_include=["model", "contrastive_loss_ratio"])
     # writer = LatexTableWriter("./tables/tweeteval/paper/cohort2", TweetEvalResult, table=TweetEvalMainTable)
     # writer.write_to_tex(name="tweeteval", session_to_include=["model"])